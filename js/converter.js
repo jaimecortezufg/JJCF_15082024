77878497a5b1e80f76be3ad94d6ef00453c0bb67
Jaime Jeovanny Cortez Flores <jaimecortez@ufg.edu.sv>
2024-08-29 07:52:32 -0600

    Finalización de código HTML, Finalización de hoja de estilos, Inicio de lógica del negocio

---
+++ js/converter.js
@@ -0,0 +1,7 @@
+const inputField = document.getElementById("input-temp");
+const fromUnitField = document.getElementById("input-unit");
+const toUnitField = document.getElementById("output-unit");
+const outputField = document.getElementById("output-temp");
+const form = document.getElementById("converter");
+
+console.log("Jaime Jeovanny Cortez Flores");
